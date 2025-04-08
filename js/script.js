362b3e9043f026102f47f1fffedbed2fcd072568
amitblogger <amitblogger999@gmail.com>
2025-04-08 09:08:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleBtn = document.getElementById('fcp-theme-toggle');
+  if (toggleBtn) {
+    toggleBtn.addEventListener('click', () => {
+      document.body.classList.toggle('fcp-dark-mode');
+    });
+  }
+});
 
